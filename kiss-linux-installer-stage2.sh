5627cd54002023369c481a1f6551b309a824dcc4
leafhy <7463804+leafhy@users.noreply.github.com>
2024-05-26 20:38:58 +0930

    update

--- kiss-linux-installer-stage2.sh
+++ kiss-linux-installer-stage2.sh
@@ -9,9 +9,19 @@ linuxfirmware="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-fi
 kissrepo="/var/db/kiss"
 kiss_cache="$kissrepo/cache"
 
-adduser "$username"
-addgroup "$username" wheel
+if [ ! "$username" ]; then
+   echo "Missing username"
+   exit
+fi
 
+user="$(getent passwd 1000 | cut -d: -f1)"
+
+if [ ! "$user" ]; then
+   adduser "$username"
+   addgroup "$username" wheel
+fi
+
+if [ ! -f "$home/.profile" ]; then
 tee $home/.profile << EOF
 export KISS_DEBUG="0"
 export KISS_SU="ssu"
@@ -24,6 +34,7 @@ export KISSREPO="/var/db/kiss"
 export KISS_PATH="\$KISSREPO/repo/core:\$KISSREPO/repo/extra:\$KISSREPO/community/community"
 alias ls="ls --color=auto"
 EOF
+fi
 
 source /root/.profile
 
@@ -56,13 +67,15 @@ source /root/.profile
 # ie. repo out of sync
 ###############################################
 
-git clone https://github.com/leafhy/repo.git $kissrepo/repo
-#git clone https://github.com/kisslinux/repo.git $kissrepo/repo
-git clone https://github.com/dylanaraps/community.git $kissrepo/community
+if [ ! -d "$kissrepo/repo" ]; then
+   git clone https://github.com/leafhy/repo.git $kissrepo/repo
+   #git clone https://github.com/kisslinux/repo.git $kissrepo/repo
+   git clone https://github.com/dylanaraps/community.git $kissrepo/community
 
-# fix git dubious permissions
-git config --global --add safe.directory "$kissrepo/repo"
-cp /root/.gitconfig "$home"
+   # fix git dubious permissions
+   git config --global --add safe.directory "$kissrepo/repo"
+   cp /root/.gitconfig "$home"
+fi
 
 kiss search \*
 
@@ -76,37 +89,36 @@ kiss search \*
 
 kiss update
 
-cp /usr/bin/kiss /usr/bin/kiss.orig
 # Change cache location to one more apt for Single User
 # and fix log permissions so builds don't fail
-if [ "$kiss_cache" ]; then
-sed "/# SOFTWARE./a\
+if [ "$kiss_cache" ] && [ ! -f "/usr/bin/kiss.orig" ]; then
+   cp /usr/bin/kiss /usr/bin/kiss.orig
+
+sed '/# SOFTWARE./a\
+\
+kiss_cache="/var/db/kiss/cache"\
 \
 uid="$(id | cut -d "(" -f 1)"\
 \
 if [ "$uid" != uid=0 ]; then\
-ssu chown -R 1000:1000 "$kiss_cache/logs"\
-fi" /usr/bin/kiss > _
+   ssu chown -R 1000:1000 "$kiss_cache/logs"\
+fi' /usr/bin/kiss > _
 mv -f _ /usr/bin/kiss
 
 sed 's/cac_dir=/#cac_dir=/g' /usr/bin/kiss > _
 mv -f _ /usr/bin/kiss
 
-sed "/Top-level cache/a\
-    cac_dir=$kiss_cache" /usr/bin/kiss > _
-mv -f _ /usr/bin/kiss
-fi
-
-# Remove nonexistant compress option
-sed 's/lzip -z/lzip/' /usr/bin/kiss > _
+sed '/Top-level cache/a\
+    cac_dir=$kiss_cache' /usr/bin/kiss > _
 mv -f _ /usr/bin/kiss
 
 chmod +x /usr/bin/kiss
+fi
 
 kiss update
 
 if [ -d "$kiss_cache" ]; then
-chown -R 1000:1000 "$kiss_cache"
+   chown -R 1000:1000 "$kiss_cache"
 fi
 
 #cd /var/db/kiss/installed && kiss build *
@@ -115,37 +127,38 @@ fi
 kiss build baseinit baselayout ssu efibootmgr intel-ucode tamsyn-font runit iproute2 zstd util-linux nasm popt
 
 if [ "$kver" ]; then
-cd "$home"
-curl -fLO "$kernel"
-tar xf "$kver.tar.xz"
-cd  "$kver"
-cp "$kissrepo/repo/linux-kernel.config" .config
-sed '/<stdlib.h>/a #include <linux/stddef.h>' tools/objtool/arch/x86/decode.c > _
-mv -f _ tools/objtool/arch/x86/decode.c
+   cd "$home"
+   curl -fLO "$kernel"
+   tar xf "$kver.tar.xz"
+   cd  "$kver"
+   cp "$kissrepo/repo/linux-kernel.config" .config
+   sed '/<stdlib.h>/a #include <linux/stddef.h>' tools/objtool/arch/x86/decode.c > _
+   mv -f _ tools/objtool/arch/x86/decode.c
 fi
 
 if [ "$kver" ] && [ -f /usr/share/doc/kiss/wiki/kernel/patches/kernel-no-perl.patch ]; then
-patch -p1 < /usr/share/doc/kiss/wiki/kernel/patches/kernel-no-perl.patch
+   patch -p1 < /usr/share/doc/kiss/wiki/kernel/patches/kernel-no-perl.patch
 fi
 
 if [ "$kver" ] && [ -f /usr/share/doc/kiss/wiki/kernel/kernel-no-perl.patch ]; then
-patch -p1 < /usr/share/doc/kiss/wiki/kernel/kernel-no-perl.patch
+   patch -p1 < /usr/share/doc/kiss/wiki/kernel/kernel-no-perl.patch
 fi
 
 chown -R 1000:1000 "$home"
 
 if [ "$lver" ]; then
-mkdir -p /usr/lib/firmware
-curl -fLO "$linuxfirmware"
-tar xf "$lver.tar.xz"
-# git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
-cp -R linux-firmware/intel /usr/lib/firmware/
+   mkdir -p /usr/lib/firmware
+   curl -fLO "$linuxfirmware"
+   tar xf "$lver.tar.xz"
+   # git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
+   cp -R linux-firmware/intel /usr/lib/firmware/
 fi
 
 echo "#####################"
 echo "#### FINAL STEPS ####"
 echo "#####################"
 echo "### Build & install kernel"
+echo "cd $home/$kver"
 echo "make && make install"
 echo "cp /boot/vmlinuz /boot/efi/vmlinuz-5.15.6"
 echo "cp /boot/System.map /boot/System.map-5.15.6"
