49ad8d59c001c392f6134e7214a3a54a4c04d982
leafhy <7463804+leafhy@users.noreply.github.com>
2024-06-08 20:51:57 +0930

    update

--- kiss-linux-installer-stage1.sh
+++ kiss-linux-installer-stage1.sh
@@ -144,14 +144,13 @@ tee /mnt/efiboot.sh << EOF
 # 'PARTUUID' is used as initramfs is required to use 'UUID'
 efibootmgr --create --disk /dev/sda --loader '\vmlinuz-$kver' --label '$efilabel' --unicode root=PARTUUID=$(blkid -s PARTUUID -o value ${device}2) loglevel=4 Page_Poison=1
 
-echo '**********************************************************'
+echo '*********************************************************************************************'
 echo -e "\x1B[1;31m [ ! ] CHECK \x1B[1;92m BootOrder: \x1B[1;31m IS CORRECT [ ! ]\x1B[1;0m"
-echo ' Boot entry needs to be towards the top of list otherwise '
-echo '       it will not appear in the boot menu                '
-echo '                                                          '
-echo '                                                          '
-echo '      Resetting BIOS will restore default boot order      '
-echo '**********************************************************'
+echo ''
+echo 'Boot entry needs to be towards the top of list otherwise it will not appear in the boot menu.'
+echo ''
+echo 'BIOS reset will restore default boot order.'
+echo '*********************************************************************************************'
 efibootmgr -v
 EOF
 fi
