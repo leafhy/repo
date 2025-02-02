c3529fb39a05ea8e56474ae499bf54c47699a258
leafhy <7463804+leafhy@users.noreply.github.com>
2025-02-02 12:03:33 +1030

    update

--- kiss-linux-installer-stage2.sh
+++ kiss-linux-installer-stage2.sh
@@ -99,8 +99,8 @@ if [ ! -f /root/.gitconfig ]; then
    git config --global --add safe.directory "$kissrepo/repo"
 
 for pkg in "$kissrepo/extra/"*; do
-   url=$(grep git+ $f/sources | grep -v '#')
-   git config --global --add safe.directory $(echo $kissrepo/cache/sources/$(basename $f)/$(basename $p))
+   url=$(grep git+ $pkg/sources | grep -v '#')
+   git config --global --add safe.directory $(echo $kissrepo/cache/sources/$(basename $pkg)/$(basename $url))
 done
 
 fi
