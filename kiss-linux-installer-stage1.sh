e9df9b57bf4dd126eff17ec294fdc38ec1021fcc
leafhy <7463804+leafhy@users.noreply.github.com>
2025-03-29 17:53:35 +1030

    update

--- kiss-linux-installer-stage1.sh
+++ kiss-linux-installer-stage1.sh
@@ -175,7 +175,7 @@ if ! [[ -d /mnt/usr ]]; then
    echo '--------------------------------------------'
 fi
 
-[[ -f $file = kiss-chroot-2021.7-9.tar.xz ]] && rm /mnt/etc/sv/{crond,syslogd}/run
+[[ $file = kiss-chroot-2021.7-9.tar.xz ]] && rm /mnt/etc/sv/{crond,syslogd}/run
 
 if ! [[ -f /mnt/root/.profile ]]; then
 tee /mnt/root/.profile << EOF >/dev/null
