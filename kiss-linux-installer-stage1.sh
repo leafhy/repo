07cd847762ca0b3619762f2a91aa8dfefe0f3702
leafhy <7463804+leafhy@users.noreply.github.com>
2026-03-12 12:26:17 +1030

    update

--- kiss-linux-installer-stage1.sh
+++ kiss-linux-installer-stage1.sh
@@ -245,17 +245,17 @@ fi
 if [[ $opt = EFI ]]; then
   tee --append /mnt/etc/fstab << EOF >/dev/null
 
-LABEL=EFI        /boot/efi    vfat    defaults    0 0
+LABEL=EFI    /boot/efi    vfat    defaults    0 0
 
 # UUID=$(blkid -s UUID -o value ${device}2)
-LABEL=$fsyslabel /           $efifsys    defaults    0 0
+LABEL=$fsyslabel    /    $efifsys    defaults    0 0
 EOF
 
 elif [[ $opt = MBR ]]; then
   tee --append /mnt/etc/fstab << EOF >/dev/null
 
 # UUID=$(blkid -s UUID -o value ${device}1)
-LABEL=$fsyslabel    $extfsys    defaults    0 0
+LABEL=$fsyslabel    /    $extfsys    defaults    0 0
 EOF
 fi
 
